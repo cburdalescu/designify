737c723aed734b3c4be3e3f7347f6b278416e7ea
Cosmin Burdalescu <cburdalescu@gmail.com>
2017-01-28 01:01:00 +0200

    factory for admin user, event migration

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,10 +15,19 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
+//    return [
+//        'name' => $faker->name,
+//        'email' => $faker->unique()->safeEmail,
+//        'password' => $password ?: $password = bcrypt('secret'),
+//        'remember_token' => str_random(10),
+//    ];
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+      'name' => 'admin',
+        'email' => 'admin@localhost.com',
+        'password' => $password ?: $password = bcrypt('admin'),
         'remember_token' => str_random(10),
+        'role' => 1,
     ];
+
 });
